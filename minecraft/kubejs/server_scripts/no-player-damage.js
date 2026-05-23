efe7c96905ee4c45590fedfae55a0a6b1b985358
HooMkA <fingerbmx2002@gmail.com>
2026-05-23 17:42:30 +0300

    v0.7.1a Изменены... а я не уверен что ща комичу не всё Короч новые скрибты для kubejs подправлены конфиги аеронавтика и энд ремастереда добавлены квесты иии...

--- minecraft/kubejs/server_scripts/no-player-damage.js
+++ minecraft/kubejs/server_scripts/no-player-damage.js
@@ -9,9 +9,18 @@ EntityEvents.beforeHurt( event => {
     // массив сущностей по которым не будет проходить урон, можно расширить
     // и проверка
     let allowedEntities = [
-        "minecraft:ender_dragon"
-        //"minecraft:zombie"
+        "minecraft:ender_dragon",
+        "minecraft:blaze",
+        "iceandfire:fire_dragon",
+        "iceandfire:ice_dragon",
+        "iceandfire:lightning_dragon"
         //"minecraft:skeleton"
+        /*"immersive_aircraft:bamboo_hopper",
+        "immersive_aircraft:airship",
+        "immersive_aircraft:biplane",
+        "immersive_aircraft:cargo_airship",
+        "immersive_aircraft:gyrodyne",
+        "immersive_aircraft:warship",*/
     ]
     if (!allowedEntities.includes(entity.getType())) {
         return
